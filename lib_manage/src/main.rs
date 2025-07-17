fdb12c978613ec23586153b447f41c4d26653fed
Pacey <pacey.cyber@gmail.com>
2025-07-17 15:15:11 -0400

    main updated and working on string vectors

--- lib_manage/src/main.rs
+++ lib_manage/src/main.rs
@@ -1,3 +1,22 @@
+// Add books to the library
+// register users
+// check out and return booksview current availablility
+// store data persistently (e.g., with JSON or SQLite)
+// Web interface using frameworks like Actix or Rocket
+
+use std::io;
+
+
+fn add_string_to_vector(){
+    let mut user_input = String::new();
+
+    io::stdin().read_line(&mut user_input);
+
+    return(user_input);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut users: Vec<&str> = ;
+    
+    users.push(add_string_to_vector());
 }
